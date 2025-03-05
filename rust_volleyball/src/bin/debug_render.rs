505c387619ac7f8d9833256348bc3779d5cb1f4d
Jakub Braz <jakubbraz@microsoft.com>
2025-03-05 11:11:58 +0100

    two players

--- rust_volleyball/src/bin/debug_render.rs
+++ rust_volleyball/src/bin/debug_render.rs
@@ -1,9 +1,8 @@
-use rust_volleyball::fn1;
 use macroquad::prelude::*;
 
 const WIDTH: f32 = 800.0;
 const HEIGHT: f32 = 600.0;
-const RESIZE_FACTOR: f32 = 200.0;
+const RESIZE_FACTOR: f32 = 100.0;
 
 fn window_conf() -> Conf {
     Conf {
@@ -29,54 +28,79 @@ async fn main() {
         }
 
         if is_key_pressed(KeyCode::Space) {
-            game_state.apply_impulse(true);
+            game_state.apply_impulse(true, true);
         }
         if is_key_pressed(KeyCode::Up) {
-            game_state.apply_impulse(false);
+            game_state.apply_impulse(false, true);
         }
         if is_key_pressed(KeyCode::Left) {
-            game_state.add_force(false);
+            game_state.add_force(false, true);
         }
         if is_key_pressed(KeyCode::Right) {
-            game_state.add_force(true);
+            game_state.add_force(true, true);
         }
-        if is_key_released(KeyCode::Left) || is_key_released(KeyCode::Right) {
-            game_state.reset_force()
+        if is_key_released(KeyCode::Left) {
+            game_state.reset_force(false, true);
+        }
+        if is_key_released(KeyCode::Right) {
+            game_state.reset_force(true, true);
+        }
+        if is_key_pressed(KeyCode::W) {
+            game_state.apply_impulse(false, false);
+        }
+        if is_key_pressed(KeyCode::A) {
+            game_state.add_force(false, false);
+        }
+        if is_key_pressed(KeyCode::D) {
+            game_state.add_force(true, false);
+        }
+        if is_key_released(KeyCode::A) {
+            game_state.reset_force(false, false);
+        }
+        if is_key_released(KeyCode::D) {
+            game_state.reset_force(true, false);
         }
 
         // UPDATE STATE
-        game_state.step();
+        game_state.step(get_frame_time());
 
         // DRAW STATE
         clear_background(Color::new(0.2, 0.5, 0.7, 1.0));
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        let (x_p, y_p, r_p) = resize_player(game_state.player1());
+        let (xp1, yp1, rp1, xp2, yp2, rp2) = game_state.players();
+        let (xp1, yp1, rp1) = resize_ball_shape((xp1, yp1, rp1));
+        let (xp2, yp2, rp2) = resize_ball_shape((xp2, yp2, rp2));
+        draw_circle(xp1, yp1, rp1, RED);
+        draw_circle(xp2, yp2, rp2, GREEN);
+        let (x_p, y_p, r_p) = resize_ball_shape(game_state.ball());
         draw_circle(x_p, y_p, r_p, YELLOW);
-        let(x_g, y_g, w_g, h_g) = resize_ground(game_state.ground());
+        let (x_g, y_g, w_g, h_g) = resize_box_shape(game_state.ground());
         draw_rectangle(x_g, y_g, w_g, h_g, BROWN);
+        let (xn, yn, wn, hn) = resize_box_shape(game_state.net());
+        draw_rectangle(xn, yn, wn, hn, BROWN);
 
-        if loop_counter % 5 == 0 {
-            println!("x: {}", x_p);
-            println!("y: {}", y_p);
-            println!("r: {}", r_p);
-            println!("{} {}", screen_width(), screen_height());
-            println!();
-            println!("ground, x: {} y: {} width: {} height: {}", x_g, y_g, w_g, h_g);
-        }
-        draw_text(&format!("Fn result = {}", fn1(10,20)), 20.0, 20.0, 30.0, DARKGRAY);
+        // if loop_counter % 5 == 0 {
+        //     println!("player1 x: {} y: {} r: {}", xp1, yp1, rp1);
+        //     println!("player2 x: {} y: {} r: {}", xp2, yp2, rp2);
+        //     println!("ball x: {} y: {} r: {}", x_p, y_p, y_p);
+        //     println!("{} {}", screen_width(), screen_height());
+        //     println!();
+        //     println!("ground, x: {} y: {} width: {} height: {}", x_g, y_g, w_g, h_g);
+        // }
 
         loop_counter += 1;
         next_frame().await
     }
 }
 
-fn resize_player(player: (f32, f32, f32)) -> (f32, f32, f32) {
+fn resize_ball_shape(player: (f32, f32, f32)) -> (f32, f32, f32) {
     let(x, y, r) = player;
     (x * RESIZE_FACTOR, y * -RESIZE_FACTOR + HEIGHT, r * RESIZE_FACTOR)
 }
 
-fn resize_ground(ground: (f32, f32, f32, f32)) -> (f32, f32, f32, f32) {
+fn resize_box_shape(ground: (f32, f32, f32, f32)) -> (f32, f32, f32, f32) {
     let (x, y, w, h) = ground;
-    ((x - w) * RESIZE_FACTOR, (y - h) * RESIZE_FACTOR + HEIGHT, w * RESIZE_FACTOR * 2.0, h * RESIZE_FACTOR * 2.0)
+    // ((x - w) * RESIZE_FACTOR, (y - h) * RESIZE_FACTOR + HEIGHT, w * RESIZE_FACTOR * 2.0, h * RESIZE_FACTOR * 2.0)
+    ((x - w) * RESIZE_FACTOR, (HEIGHT / RESIZE_FACTOR - y - h) * RESIZE_FACTOR, w * RESIZE_FACTOR * 2.0, h * RESIZE_FACTOR * 2.0)
 }
--- rust_volleyball/src/lib.rs
+++ rust_volleyball/src/lib.rs
@@ -1,9 +1,11 @@
-use rapier2d::na::SMatrix;
+use std::collections::HashMap;
 use rapier2d::prelude::*;
 
-pub fn fn1(a: i32, b: i32) -> i32 {
-    a + b
-}
+const TIME_STEP: f32 = 1.0 / 60.0;
+
+const MAX_SPEED: f32 = 3.0;
+const ALMOST_ZERO: f32 = 0.001;
+const MOVE_FORCE: f32 = 10.0;
 
 pub struct GameState {
     rigid_body_set: RigidBodySet,
@@ -21,8 +23,21 @@ pub struct GameState {
     physics_hooks: (),
     event_handler: (),
 
+    time: f32,
+    game_time: f32,
     player1_handle: RigidBodyHandle,
+    player2_handle: RigidBodyHandle,
+    ball_handle: RigidBodyHandle,
     ground_handle: ColliderHandle,
+    net_handle: ColliderHandle,
+    player1_collider_handle: ColliderHandle,
+    player2_collider_handle: ColliderHandle,
+    ball_collider_handle: ColliderHandle,
+    left_wall_handle: ColliderHandle,
+    right_wall_handle: ColliderHandle,
+    middle_wall_handle: ColliderHandle,
+
+    player_input: HashMap<RigidBodyHandle, [bool; 2]>,
 }
 
 impl GameState {
@@ -43,67 +58,241 @@ impl GameState {
             physics_hooks: (),
             event_handler: (),
 
+            time: 0.0,
+            game_time: 0.0,
             player1_handle: RigidBodyHandle::default(),
+            player2_handle: RigidBodyHandle::default(),
+            ball_handle: RigidBodyHandle::default(),
             ground_handle: ColliderHandle::default(),
+            net_handle: ColliderHandle::default(),
+            player1_collider_handle: ColliderHandle::default(),
+            player2_collider_handle: ColliderHandle::default(),
+            ball_collider_handle: ColliderHandle::default(),
+            left_wall_handle: ColliderHandle::default(),
+            right_wall_handle: ColliderHandle::default(),
+            middle_wall_handle: ColliderHandle::default(),
+
+            player_input: HashMap::new(),
         };
 
-        /* Create the ground. */
-        let collider = ColliderBuilder::cuboid(10.0, 0.1).build();
-        game_state.ground_handle = game_state.collider_set.insert(collider);
-
-        /* Create the bouncing ball. */
-        let rigid_body = RigidBodyBuilder::dynamic()
-            .translation(vector![0.0, 10.0])
+        // Create walls.
+        let collider = ColliderBuilder::cuboid(4.0, 0.1)
+            .translation(vector![4.0, 0.0])
             .build();
-        let collider = ColliderBuilder::ball(0.5).restitution(0.7).build();
+        game_state.ground_handle = game_state.collider_set.insert(collider);
+        let collider = ColliderBuilder::cuboid(0.1, 200.0)
+            .translation(vector![8.0, 200.0])
+            .friction(0.0)
+            .friction_combine_rule(CoefficientCombineRule::Min)
+            .build();
+        game_state.right_wall_handle = game_state.collider_set.insert(collider);
+        let collider = ColliderBuilder::cuboid(0.1, 200.0)
+            .translation(vector![0.0, 200.0])
+            .friction(0.0)
+            .friction_combine_rule(CoefficientCombineRule::Min)
+            .build();
+        game_state.left_wall_handle = game_state.collider_set.insert(collider);
+        let collider = ColliderBuilder::cuboid(0.1, 200.0)
+            .translation(vector![4.0, 200.0])
+            .collision_groups(InteractionGroups::new(Group::GROUP_2, Group::GROUP_2))
+            .friction(0.0)
+            .friction_combine_rule(CoefficientCombineRule::Min)
+            .build();
+        game_state.middle_wall_handle = game_state.collider_set.insert(collider);
+
+        // Create net
+        let collider = ColliderBuilder::cuboid(0.05, 1.0)
+            .translation(vector![4.0, 1.0])
+            .build();
+        game_state.net_handle = game_state.collider_set.insert(collider);
+
+        // Create the players
+        let rigid_body = RigidBodyBuilder::dynamic()
+            .translation(vector![6.0, 3.0])
+            .build();
+        let collider = ColliderBuilder::ball(0.5)
+            .restitution(0.7)
+            .restitution_combine_rule(CoefficientCombineRule::Min)
+            .build();
         game_state.player1_handle = game_state.rigid_body_set.insert(rigid_body);
-        game_state.collider_set.insert_with_parent(collider, game_state.player1_handle, &mut game_state.rigid_body_set);
+        let handle = game_state.collider_set.insert_with_parent(collider, game_state.player1_handle, &mut game_state.rigid_body_set);
+        game_state.player1_collider_handle = handle;
+
+        let rigid_body = RigidBodyBuilder::dynamic()
+            .translation(vector![2.0, 3.0])
+            .build();
+        let collider = ColliderBuilder::ball(0.5)
+            .restitution(0.7)
+            .restitution_combine_rule(CoefficientCombineRule::Min)
+            .build();
+        game_state.player2_handle = game_state.rigid_body_set.insert(rigid_body);
+        let handle = game_state.collider_set.insert_with_parent(collider, game_state.player2_handle, &mut game_state.rigid_body_set);
+        game_state.player2_collider_handle = handle;
+
+        game_state.player_input.insert(game_state.player1_handle, [false; 2]);
+        game_state.player_input.insert(game_state.player2_handle, [false; 2]);
+
+        // Create ball
+        let rigid_body = RigidBodyBuilder::dynamic()
+            .translation(vector![4.0, 3.0])
+            .build();
+        let collider = ColliderBuilder::ball(0.25)
+            .restitution(0.8)
+            .density(0.9)
+            .restitution_combine_rule(CoefficientCombineRule::Max)
+            .collision_groups(InteractionGroups::new(Group::GROUP_1, Group::GROUP_1))
+            .build();
+        game_state.ball_handle = game_state.rigid_body_set.insert(rigid_body);
+        let handle = game_state.collider_set.insert_with_parent(collider, game_state.ball_handle, &mut game_state.rigid_body_set);
+        game_state.ball_collider_handle = handle;
 
         game_state
     }
 
-    pub fn player1(&self) -> (f32, f32, f32) {
-        let body = &self.rigid_body_set[self.player1_handle];
+    pub fn players(&self) -> (f32, f32, f32, f32, f32, f32) {
+        let body1 = &self.rigid_body_set[self.player1_handle];
+        let t1 = body1.translation();
+        let r1 = self.collider_set[body1.colliders()[0]].shape().as_ball().unwrap().radius;
+        let body2 = &self.rigid_body_set[self.player2_handle];
+        let t2 = body2.translation();
+        let r2 = self.collider_set[body2.colliders()[0]].shape().as_ball().unwrap().radius;
+        (t1.x, t1.y, r1, t2.x, t2.y, r2)
+    }
+
+    pub fn ball(&self) -> (f32, f32, f32) {
+        let body = &self.rigid_body_set[self.ball_handle];
         let r = self.collider_set[body.colliders()[0]].shape().as_ball().unwrap().radius;
         (body.translation().x, body.translation().y, r)
     }
 
     pub fn ground(&self) -> (f32, f32, f32, f32) {
-        let size = &self.collider_set[self.ground_handle].shape().as_cuboid().unwrap().half_extents;
         let pos = self.collider_set[self.ground_handle].translation();
+        let size = &self.collider_set[self.ground_handle].shape().as_cuboid().unwrap().half_extents;
         (pos.x, pos.y, size.x, size. y)
     }
 
-    pub fn step(&mut self) {
-        self.physics_pipeline.step(
-            &self.gravity,
-            &self.integration_parameters,
-            &mut self.island_manager,
-            &mut self.broad_phase,
-            &mut self.narrow_phase,
-            &mut self.rigid_body_set,
-            &mut self.collider_set,
-            &mut self.impulse_joint_set,
-            &mut self.multi_body_joint_set,
-            &mut self.ccd_solver,
-            Some(&mut self.query_pipeline),
-            &self.physics_hooks,
-            &self.event_handler,
-        )
+    pub fn net(&self) -> (f32, f32, f32, f32) {
+        let pos = self.collider_set[self.net_handle].translation();
+        let size = &self.collider_set[self.net_handle].shape().as_cuboid().unwrap().half_extents;
+        (pos.x, pos.y, size.x, size. y)
     }
 
-    pub fn apply_impulse(&mut self, is_strong: bool) {
-        let body = &mut self.rigid_body_set[self.player1_handle];
-        body.apply_impulse(vector![0.0, if is_strong { 10.0 } else { 5.0 }], true);
+    pub fn step(&mut self, frame_time: f32) {
+        self.time += frame_time;
+        self.game_time += frame_time;
+        // let mut c = 0;
+        while self.time - TIME_STEP > 0.0 {
+            // c += 1;
+            self.time -= TIME_STEP;
+
+            self.control_player(self.player1_handle, self.player1_collider_handle, self.middle_wall_handle, self.right_wall_handle);
+            self.control_player(self.player2_handle, self.player2_collider_handle, self.left_wall_handle, self.middle_wall_handle);
+
+            self.physics_pipeline.step(
+                &self.gravity,
+                &self.integration_parameters,
+                &mut self.island_manager,
+                &mut self.broad_phase,
+                &mut self.narrow_phase,
+                &mut self.rigid_body_set,
+                &mut self.collider_set,
+                &mut self.impulse_joint_set,
+                &mut self.multi_body_joint_set,
+                &mut self.ccd_solver,
+                Some(&mut self.query_pipeline),
+                &self.physics_hooks,
+                &self.event_handler,
+            );
+        }
+        // println!("counter {}", c);
+
+        // let player1_body = &self.rigid_body_set[self.player1_handle];
+        // println!("ang vel: {}", player1_body.angvel());
+        // println!("line vel: {}", player1_body.linvel());
+        // println!("forces {}", player1_body.user_force());
+        // println!("time {}", self.game_time);
     }
 
-    pub fn add_force(&mut self, right_force: bool) {
-        let body = &mut self.rigid_body_set[self.player1_handle];
-        body.add_force(vector![1.0 * (if right_force { 1.0 } else { -1.0 }), 0.0], true);
+    fn control_player(&mut self, handle: RigidBodyHandle, collider: ColliderHandle, left: ColliderHandle, right: ColliderHandle) {
+        let player_body = &mut self.rigid_body_set[handle];
+        let pressing_left = self.player_input[&handle][0];
+        let pressing_right = self.player_input[&handle][1];
+        player_body.set_angvel(0.0, true);
+        let v = *player_body.linvel();
+        let f = player_body.user_force().x;
+
+        if pressing_right && f == 0.0 {
+            player_body.add_force(vector![MOVE_FORCE, 0.0], true);
+        }
+        else if pressing_left && f == 0.0 {
+            player_body.add_force(vector![-MOVE_FORCE, 0.0], true);
+        }
+
+        if v.x.abs() > MAX_SPEED {
+            player_body.set_linvel(vector![if v.x > 0.0 { MAX_SPEED } else { -MAX_SPEED }, v.y], true);
+            player_body.reset_forces(true);
+        }
+        if !pressing_left && !pressing_right {
+            player_body.reset_forces(true);
+        }
+
+        if v.x.abs() < ALMOST_ZERO {
+            player_body.set_linvel(vector![0.0, v.y], true);
+        }
+
+        if contact(&self.narrow_phase, collider, right) && pressing_right || contact(&self.narrow_phase, collider, left) && pressing_left {
+            player_body.reset_forces(true);
+        }
     }
 
-    pub fn reset_force(&mut self) {
-        let body = &mut self.rigid_body_set[self.player1_handle];
-        body.reset_forces(true);
+    pub fn apply_impulse(&mut self, is_strong: bool, is_player1: bool) {
+        if !is_strong {
+            let (handler, coll_handler) = if is_player1 {
+                (self.player1_handle, self.player1_collider_handle)
+            } else {
+                (self.player2_handle, self.player2_collider_handle)
+            };
+            if contact(&self.narrow_phase, coll_handler, self.ground_handle) {
+                let body = &mut self.rigid_body_set[handler];
+                body.apply_impulse(vector![0.0, 5.0], true);
+            }
+        }
+        else {
+            let body = &mut self.rigid_body_set[self.ball_handle];
+            body.apply_impulse(vector![0.0, 1.0], true);
+        }
     }
+
+    pub fn add_force(&mut self, right_force: bool, is_player1: bool) {
+        // let body = &mut self.rigid_body_set[self.player1_handle];
+        // body.add_force(vector![MOVE_FORCE * (if right_force { 1.0 } else { -1.0 }), 0.0], true);
+        let handle = if is_player1 { self.player1_handle } else { self.player2_handle };
+        let [left, right] = self.player_input[&handle];
+        if right_force {
+            self.player_input.insert(handle, [left, true]);
+        }
+        else {
+            self.player_input.insert(handle, [true, right]);
+        }
+    }
+
+    pub fn reset_force(&mut self, right_force: bool, is_player1: bool) {
+        // let body = &mut self.rigid_body_set[self.player1_handle];
+        // body.reset_forces(true);
+        let handle = if is_player1 {self.player1_handle} else {self.player2_handle};
+        let [left, right] = self.player_input[&handle];
+        if right_force {
+            self.player_input.insert(handle, [left, false]);
+        }
+        else {
+            self.player_input.insert(handle, [false, right]);
+        }
+    }
+}
+
+fn contact(narrow_phase: &NarrowPhase, handle1: ColliderHandle, handle2: ColliderHandle) -> bool {
+    if let Some(pair) = narrow_phase.contact_pair(handle1, handle2) {
+        return pair.has_any_active_contact;
+    }
+    false
 }
